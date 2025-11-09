2ac872d3a1dda0875ce7090fa1e53bcf81da48ae
Pantek <pantekjaya65@gmail.com>
2025-11-09 18:49:15 +0700

    Push Migrations

--- src/database/migrations/2025_11_09_000000_create_orca_menu_table.php
+++ src/database/migrations/2025_11_09_000000_create_orca_menu_table.php
@@ -8,6 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
+        //SCHEMA ORCA MENU
         Schema::create('orca_menu', function (Blueprint $table) {
             $table->id();
             $table->string('nama_menu');
